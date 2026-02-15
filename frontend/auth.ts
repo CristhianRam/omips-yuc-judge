a43dc2881dc06820acfd99e44a31fabdbb629abb
Saul Canche <saulcanche.sc8@gmail.com>
2026-02-15 16:02:27 -0600

    migrated login to api

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -3,42 +3,50 @@ import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
-import bcrypt from 'bcrypt';
-import postgres from 'postgres';
- 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
- 
-async function getUser(email: string): Promise<User | undefined> {
-  try {
-    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
-    return user[0];
-  } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
-  }
-}
- 
+
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
     Credentials({
-      async authorize(credentials) {
-        const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string().min(6) })
-          .safeParse(credentials);
- 
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-          const user = await getUser(email);
-          if (!user) return null;
-          //console.log("user Found, ", user);
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-          //console.log("do password match?", passwordsMatch);
-          if(passwordsMatch) return user;
+          async authorize(credentials) {
+          const parsedCredentials = z
+            .object({ username: z.string().min(1), password: z.string().min(6) })
+            .safeParse(credentials);
+            
+          if (parsedCredentials.success) {
+            const { username, password } = parsedCredentials.data;
+            const formData = new FormData();
+            formData.append('username', username);
+            formData.append('password', password);
+            
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API}/auth/token`, {
+              method: 'POST',
+              body: formData,
+            });
+            
+            if (!response.ok) return null;
+            
+            console.log("response status: ", response.status);
+            const data = await response.json();
+            const token = data.access_token;
+            
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API}/users/me`, {
+              headers: {
+                'Authorization': `Bearer ${token}`,
+              }
+            });
+            
+            const userData = await res.json();
+            
+            return {
+              token,
+              ...userData
+            };
+          }
+          
+          console.log('Invalid credentials');
+          return null;
         }
-        console.log('Invalid credentials');
-        return null;
-      },
     }),
   ],
 });
